6d020232d6bbe207fd944f507e6c00843356f2aa
Lawted <lawtedwu@gmail.com>
2025-06-10 03:15:15 +0800

    feat: add global styles and layout enhancements for dashboard
    - Introduced a new globals.css file to define custom CSS variables and styles for the dashboard.
    - Updated the layout component to include metadata and removed unused pathname logic for cleaner code.
    - Enhanced styling for dark mode and added support for Safari safe area insets to improve layout on various devices.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import type { Metadata } from "next";
 import "./globals.css";
-import { usePathname } from "next/navigation";
 
 import { AppSidebar } from "@/components/app-sidebar";
 import { Toaster } from "@/components/ui/sonner";
@@ -21,35 +18,23 @@ const shantellSans = Shantell_Sans({
   variable: "--font-shantell-sans",
 });
 
-// export const metadata: Metadata = {
-//   title: "AI Platform",
-//   description: "AI Platform Management System",
-//   icons: {
-//     icon: [
-//       { url: "/favicon.ico", sizes: "any" },
-//       { url: "/Favicon64x64.png", sizes: "64x64" },
-//     ],
-//     apple: "/apple-touch-icon.png",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "AI Platform",
+  description: "AI Platform Management System",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/Favicon64x64.png", sizes: "64x64" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  if (pathname.includes("pua-game-mobile")) {
-    return (
-      <html lang="en">
-        <body
-          className={`${londrinaSolid.variable} ${shantellSans.variable}`}
-        >
-          {children}
-        </body>
-      </html>
-    );
-  }
   return (
     <html lang="en">
       <body className={`${londrinaSolid.variable} ${shantellSans.variable}`}>
